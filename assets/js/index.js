d6ca368744545cc4ab28ed46fa463a9f0e1c2201
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-08-26 23:55:40 -0300

    feat: add function show experiences

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,4 +3,34 @@ let menu = document.querySelector('.menu')
 
 toggle.onclick = function(){
   menu.classList.toggle('active')
-}
+};
+
+/*
+function buttonShowContent(){
+  let content = document.getElementById("content");
+  content.onclick = function(){
+    if(content.style.display === "none"){
+      content.style.display = "block";
+    }else{
+      content.style.display = "none";
+    }
+
+  }
+};
+*/
+
+let btn = document.querySelectorAll('button')
+let tabs = document.querySelectorAll('.tabs > div')
+
+btn.forEach(function(click){
+  click.addEventListener('click', function(){
+    tabs.forEach(function(tab){
+      tab.classList.add('content-tabs')
+    })
+
+    let tabId = click.getAttribute('data-id')
+    let tab = document.querySelector("#" + tabId)
+    tab.classList.remove('content-tabs')
+
+  })
+})
