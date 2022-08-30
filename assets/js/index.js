a312bf2ba481ce1178421e0775dc898c0fc952b1
Pedro Nunes <adm.pedronunes@hotmail.com>
2022-08-29 22:18:35 -0300

    feat: active links and responsivity

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,4 +19,13 @@ btn.forEach(function(click){
     tab.classList.remove('content-tabs')
 
   })
-})
+});
+
+const list = document.querySelectorAll('.list');
+function activeLink(){
+    list.forEach((item) =>
+    item.classList.remove('active'));
+    this.classList.add('active');
+}
+list.forEach((item) =>
+item.addEventListener('click', activeLink));
